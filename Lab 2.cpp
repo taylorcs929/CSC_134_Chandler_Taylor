b93b15ed9e4b6840334743def480735ac14f7e73
SANDHILLS\LH218-19 <jad.talbert@gmail.com>
2025-08-27 12:18:12 -0400

    created files for lab

---
+++ Lab 2.cpp
@@ -0,0 +1,9 @@
+//@author: Chandler Taylor
+//@ 08/27/2025
+//@purpose: This program calculates the distance travelled (x) given the following
+
+//include the iostream 
+#include <iostream>
+
+//use standard namespace 
+using namespace std; 
---
+++ product_of_two_numbers.cpp
@@ -0,0 +1,23 @@
+//@author: Chandler Taylor
+//@ 08/27/2025
+//@purpose: This program calculates the product of two numbers
+
+//include the iostream directive
+#include <iostream>
+
+//use the standard namespace
+using namespace std;
+
+//define the main() function/method
+int main()
+{
+    int num1 = 5;
+    int num2 =10;
+    int product = num1 * num2;
+
+    //this prints out the product of the two numbers from above.
+    std::cout << product<< std:: endl;
+
+    return 0; 
+
+}
