8ff65a0ff7905d1a698e637f7b4e09700458ada2
taylorcs929 <taylorcs929@mail.sandhills.edu>
2025-11-30 19:00:48 -0500

    Simplify purpose comment in week15lab.cpp
    Removed detailed purpose comment and retained the basic description.

--- c++/week15lab.cpp
+++ c++/week15lab.cpp
@@ -1,6 +1,6 @@
 //author@ Chandler Taylor
 //date@ 11/29/2025
-//purpose@ You should use the existing code provided in TinkerCad and properly build a 2-D array that represents the keypad. You should use the "keys" character array to store the keypad---this variable is already represented in the code for you. You should determine which key is pressed(e.g. 1,2,3, A,B,C etc.) and print it to the TinkerCad console, so that you can visualize the keypress event. You should create a passcode variable at least 5 characters in length and determine if the passcode you entered is valid. Note: the pound/hash(#) key should represent the "Enter Key", meaning that when you press #, that is the end of the passcode and it should be validated after the # key is pressed. If the passcode is correct, then you should turn on the green LED for 3 seconds. If the passcode is invalid, you should turn on the red LED for 3 seconds. You should use conditional logic and any other logic constructs(e.g. for-loops) to accomplish this simple security system.
+//purpose@ You should use the existing code provided in TinkerCad and properly build a 2-D array that represents the keypad.
 
 #include <iostream>
 using namespace std;
